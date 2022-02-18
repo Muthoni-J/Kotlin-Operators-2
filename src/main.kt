88e6536aad11e8f9fb743c5adcef2ba93ec0ae78
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-02-18 21:31:29 +0300

    Add my file

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+fun main(){
+    name()
+    remainder()
+    sum()
+    interesting()
+}
+fun name(){
+    val name = "Joan"
+    println("Hello "+name)
+}
+fun remainder(){
+    var a = 111
+    var b = 7
+    var remainder = a%b
+    println(remainder)
+}
+fun sum(){
+    var j = 10
+    var k = 20
+    var l = 30
+    var m = 40
+    var sum = j+k+l+m
+    println(sum)
+}
+fun interesting(){
+    println("I am a designer same time a model")
+}
